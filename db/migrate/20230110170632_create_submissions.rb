de07fddb9ac2c37a1de0b29d9c837af174cc0135
jy-ko <jclareko@gmail.com>
2023-01-10 19:30:10 +0200

    init with pico

---
+++ db/migrate/20230110170632_create_submissions.rb
@@ -0,0 +1,9 @@
+class CreateSubmissions < ActiveRecord::Migration[7.0]
+  def change
+    create_table :submissions do |t|
+      t.references :user, null: false, foreign_key: true
+
+      t.timestamps
+    end
+  end
+end
